d4515262d4a9508338d62ddf825979ccb35d4621
Aurelien Dobbels <adobbels@gmail.com>
2017-11-21 14:23:25 +0100

    exo done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,22 +9,31 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
   end
 
   def edit
   end
 
   def update
+    @task.update(task_params)
   end
 
   def destroy
     @task.destroy
+    redirect_to tasks_path
   end
 
 private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
 
   def set_task
     @task = Task.find(params[:id])
