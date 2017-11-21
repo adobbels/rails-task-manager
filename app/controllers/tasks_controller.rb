28c803cd956d821096a3a87cc217b1609d860875
Aurelien Dobbels <adobbels@gmail.com>
2017-11-21 12:32:31 +0100

    Add description to task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,33 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121102031) do
+ActiveRecord::Schema.define(version: 20171121113038) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
 end
